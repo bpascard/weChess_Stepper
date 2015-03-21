3e87d07a3171d3127139baa110206b079bbba80e
bpascard <bpascard@hotmail.com>
2015-03-21 20:43:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+git clone git://git.drogon.net/wiringPi $DIR
 g++ -Wall -o ./move ./move.cpp Position.cpp Deplacement.cpp -lwiringPi
 while [ 1 ]
 do
